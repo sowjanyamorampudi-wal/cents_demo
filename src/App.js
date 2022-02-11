d7f274ebc468a176e6ec7ca0a121be207ef49766
sowjanya <sowjanya.morampudi@westaglielabs.com>
2022-02-11 11:30:23 +0530

    okk

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Taxes from "./components/adminComponent/adminSidebar/account/taxes/taxes"
 import Settings from "./components/adminComponent/adminSidebar/account/settings/settings";
 
 function App() {
-  // con
+  // con hiiiiii
   return (
     <div>
       <BrowserRouter>
